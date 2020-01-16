696b2879b2cdee645d095ecf318ef1c6721955d6
Niko <niko_25_@hotmail.com>
2020-01-16 20:47:55 +0000

    set new default stylesheet

--- config/page.php
+++ config/page.php
@@ -19,7 +19,7 @@ return [
             "lang" => "sv",
             "stylesheets" => [
                 // Change here to set another default stylesheet
-                "css/kmom06_designprinciper.css",
+                "css/kmom06-designprinciper.css",
                 //"css/kmom01.css",
             ],
             "javascripts" => [
